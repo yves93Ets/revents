7025c1c3f31d29e3274ac71917bbf5c84efb3f9f
yves93ets <alain-yves.gabriel-mireles.1@ens.etsmtl.ca>
2018-11-05 19:50:02 -0500

    end of section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
-
+import EventDashboard from "../../features/event/eventDashbord/EventDashboard";
+import NavBar from "../../features/nav/navBar/NavBar";
+import { Container } from "semantic-ui-react";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Re-vents</h1>
+      <div>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
       </div>
     );
   }
---
+++ src/features/event/eventList/EventList.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import EventListItem from "./EventListItem";
+class EventList extends Component {
+  state = {};
+  render() {
+    const { events } = this.props;
+    return (
+      <div>
+        {events.map(event => (
+          <EventListItem key={event.id} event={event} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default EventList;
